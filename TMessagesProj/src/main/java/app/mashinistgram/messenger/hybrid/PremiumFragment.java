07c1eda0075b0b6708e76ca5701f6544686ec8cc
mashinistTEP <kirillfillpromortis@gmail.com>
2026-05-17 19:35:53 +0300

    Add Kotlin support and real activities

--- TMessagesProj/src/main/java/app/mashinistgram/messenger/hybrid/PremiumFragment.java
+++ TMessagesProj/src/main/java/app/mashinistgram/messenger/hybrid/PremiumFragment.java
@@ -1,17 +1,17 @@
 package app.mashinistgram.messenger.hybrid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import org.telegram.ui.ActionBar.BaseFragment;
 
 public class PremiumFragment extends BaseFragment {
+    @Override
     public View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView textView = new TextView(getContext());
-        textView.setText("Premium - скоро будет!");
-        textView.setTextSize(20);
-        return textView;
+        Intent intent = new Intent(getContext(), PremiumActivity.class);
+        getContext().startActivity(intent);
+        return null;
     }
 }
